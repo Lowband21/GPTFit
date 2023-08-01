6c95dffdd17f04b0c466053d9c2b32f79fc4ef2b
Lowband <GraysonHieb21@Gmail.com>
2023-08-01 11:46:43 -0600

    Partial profile saving

--- server.py
+++ server.py
@@ -225,57 +225,61 @@ from flask_login import login_required, current_user
 @app.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile(): 
-    if request.method == 'GET':
-        profile = FitnessProfile.query.filter_by(user_id=current_user.id).first()
-        if profile:
-            # Convert JSON strings to list
-            favorite_exercises = json.loads(profile.favorite_exercises)
-            exercise_blacklist = json.loads(profile.exercise_blacklist)
-            days_cant_train = json.loads(profile.days_cant_train)
-            equipment = json.loads(profile.equipment)
+    try:
+        if request.method == 'GET':
+            profile = FitnessProfile.query.filter_by(user_id=current_user.id).first()
+            if profile:
+                # Convert JSON strings to list
+                favorite_exercises = json.loads(profile.favorite_exercises)
+                exercise_blacklist = json.loads(profile.exercise_blacklist)
+                days_cant_train = json.loads(profile.days_cant_train)
+                equipment = json.loads(profile.equipment)
 
-            return jsonify(
-                name=profile.name,
-                age=profile.age,
-                height=profile.height,
-                weight=profile.weight,
-                gender=profile.gender,
-                years_trained=profile.years_trained,
-                type=profile.type,
-                fitness_level=profile.fitness_level,
-                injuries=profile.injuries,
-                fitness_goal=profile.fitness_goal,
-                target_timeframe=profile.target_timeframe,
-                challenges=profile.challenges,
-                favorite_exercises=favorite_exercises,
-                exercise_blacklist=exercise_blacklist,
-                frequency=profile.frequency,
-                days_cant_train=days_cant_train,
-                preferred_workout_duration=profile.preferred_workout_duration,
-                gym_or_home=profile.gym_or_home,
-                equipment=equipment
-            )
-        else:
-            return jsonify(error='No profile found'), 404
-    else: # POST request
-        data = request.get_json()
-        profile = FitnessProfile.query.filter_by(user_id=current_user.id).first()
-        if profile:
-            for key, value in data.items():
-                # Convert list to JSON string
-                if key in ["favorite_exercises", "exercise_blacklist", "days_cant_train", "equipment"]:
-                    value = json.dumps(value)
-                setattr(profile, key, value)
-        else: # if profile does not exist, create it
-            # Convert lists to JSON strings
-            for key in ["favorite_exercises", "exercise_blacklist", "days_cant_train", "equipment"]:
-                if key in data:
-                    data[key] = json.dumps(data[key])
-            profile = FitnessProfile(user_id=current_user.id, **data)
-            db.session.add(profile)
-        
-        db.session.commit()
-        return jsonify(success=True)
+                return jsonify(
+                    name=profile.name,
+                    age=profile.age,
+                    height=profile.height,
+                    weight=profile.weight,
+                    gender=profile.gender,
+                    years_trained=profile.years_trained,
+                    type=profile.type,
+                    fitness_level=profile.fitness_level,
+                    injuries=profile.injuries,
+                    fitness_goal=profile.fitness_goal,
+                    target_timeframe=profile.target_timeframe,
+                    challenges=profile.challenges,
+                    favorite_exercises=favorite_exercises,
+                    exercise_blacklist=exercise_blacklist,
+                    frequency=profile.frequency,
+                    days_cant_train=days_cant_train,
+                    preferred_workout_duration=profile.preferred_workout_duration,
+                    gym_or_home=profile.gym_or_home,
+                    equipment=equipment
+                )
+            else:
+                return jsonify(error='No profile found'), 404
+        else: # POST request
+            data = request.get_json()
+            profile = FitnessProfile.query.filter_by(user_id=current_user.id).first()
+            if profile:
+                for key, value in data.items():
+                    # Convert list to JSON string
+                    if key in ["favorite_exercises", "exercise_blacklist", "days_cant_train", "equipment"]:
+                        value = json.dumps(value)
+                    if isinstance(value, int) or value is None:
+                        setattr(profile, key, value)
+            else: # if profile does not exist, create it
+                # Convert lists to JSON strings
+                for key in ["favorite_exercises", "exercise_blacklist", "days_cant_train", "equipment"]:
+                    if key in data:
+                        data[key] = json.dumps(data[key])
+                profile = FitnessProfile(user_id=current_user.id, **data)
+                db.session.add(profile)
+            
+            db.session.commit()
+            return jsonify(success=True)
+    except Exception as e:
+        return jsonify(error=str(e)), 500
 
 @app.route('/auth_status', methods=['GET'])
 def auth_status():
